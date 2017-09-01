2470cb8a692bc67aa8a2178fb972bf1e805e13ad
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-08-31 22:16:42 -0400

    Updated java file

--- parse.java
+++ parse.java
@@ -7,9 +7,11 @@ public class parse
 {
 	public static void main(String[] args)
 	{
+		//Variable declarations
 		int word = 0;
 		int syllable = 0;
 		int sentence = 0;
+		String vowel = "aeiouy";
 		double alpha = 0.0;
 		double beta = 0.0;
 		try
@@ -23,6 +25,21 @@ public class parse
 				//If statement that checks for sentences 
 				if(line.contains(".") || line.contains(":") || line.contains(";") || line.contains("?") || line.contains("!"))
 					sentence++;
+				//For loop to go through length of a word and count # of syylables
+				for(int i = 0; i < line.length(); i++)
+				{
+					//Checks if the current char is a vowel
+					if(Vowel(line.charAt(i)))
+					{
+						syllable++;
+						//Checks if next letter is syllable
+						if(Vowel(line.charAt(i+1)));
+							syllable--;
+					}
+					//Checks if the word ends with an e
+					if(i == line.length()-1 && line.charAt(i) == 'e')
+						syllable--;
+				}
 				word++;
 			}
 			file.close();
@@ -34,7 +51,16 @@ public class parse
 		double grade = Math.round(((alpha * 11.8) - (beta * .39) - 15.59) * 10) / 10;
 		//System.out.println("Number of words in file: " + word);
 		//System.out.println("Number of sentences: " + sentence);
+		System.out.println("Number of syllables: " + syllable);
 		System.out.println("Flesch Index: " + index);
 		System.out.println("Flesch Grade: " + grade);
+	}
+	//Method that checks all vowels and returns true if is a vowel
+	public static boolean Vowel(char v)
+	{
+		if(v == 'a' || v == 'e' || v == 'i' || v == 'o'|| v == 'u' || v == 'y')
+			return true;
+		else
+			return false;
 	}
 }
