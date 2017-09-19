f76d32e4ae141d9ba926de9489cdf035a68ad3ab
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-09-19 11:35:24 -0400

    Final Perl

--- perl/parse3.pl
+++ perl/parse3.pl
@@ -3,32 +3,18 @@ use POSIX;
 
 my $filename = $ARGV[0];
 my($word,$syllable,$sentence) = (0,0,0);
+#Opens the file entered
 open(FILE, "+<$filename") or die "Could not open file: '$filename' $!";
-@vowels = ("a","e","i","o","u","y","A","E","I","O","U","Y");
 my ($line);
 @line = <FILE>;
 foreach $ch (@line)
 {
       #Checks the number of words in file
       $word += scalar(split(/\W+/, $ch));
-      #Checks for any punctuation on the line
+      #Checks for any punctuation
       $sentence += @punct = $ch =~ /[.!?;:]+/g;
-      #Checks for vowels
-      foreach (split //,$ch)
-      {
-            if($ch =~ "a" && $ch =~ "e" && $ch =~ "i" && $ch =~ "o" && $ch =~ "u" && $ch =~ "y")
-            {
-                  $syllable++;
-                  if($ch+1 =~ "a" && $ch+1 =~ "e" && $ch+1 =~ "i" && $ch+1 =~ "o" && $ch+1 =~ "u" && $ch+1 =~ "y")
-                  {
-                        $syllable--;
-                  }
-            }
-            elsif($ch =~ "A" && $ch =~ "E" && $ch =~ "I" && $ch =~ "O" && $ch =~ "U" && $ch =~ "Y")
-            {
-                  $syllable++;
-            }
-      }
+      #Checks for vowels and does syllable rules
+      $syllable += $ch =~ s/(?!e[ds]?$)[aeiouy]+//g;
 }
 close(FILE);
 $alpha = $syllable/$word;
@@ -38,8 +24,8 @@ $index = (206.835 - ($alpha*84.6) - ($beta*1.015));
 $roundedindex = sprintf "%.0f", $index;
 $grade = ($alpha*11.8) + ($beta*.39) - 15.59;
 $roundedgrade = sprintf "%.1f", $grade;
-print("Number of words: $word\n");
-print("Number of sentences: $sentence\n");
-print("Number of syllables: $syllable\n");
+#print("Number of words: $word\n");
+#print("Number of sentences: $sentence\n");
+#print("Number of syllables: $syllable\n");
 print("Index: $roundedindex\n");
 print("Grade: $roundedgrade\n");
