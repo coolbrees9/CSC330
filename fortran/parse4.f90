52b756d01994001d18574cc3431d3bb94657ecca
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-09-21 14:42:17 -0400

    Getting a duplicate loop error

--- fortran/parse4.f90
+++ fortran/parse4.f90
@@ -1,70 +1,15 @@
 program reader
 implicit none
 
-character, dimension(:),allocatable::long_string
 integer::filesize
 !Variable declarations
 integer(8), parameter::length=5000000
-integer::ierr,nwords,i,j
-character,dimension(:),allocatable::word(:)
-real::syllable= 0.0
-real::sentence= 0.0
+integer::pos,pos2,pos3,i,j,k,nwords,nsentence,nsyllable
 real::alpha, beta, findex, grade
 
-interface
-subroutine read_file(string,filesize)
-character,dimension(:),allocatable::string
-integer::filesize
-end subroutine read_file
-end interface
-
-call read_file(long_string,filesize)
-!Counts words
-word=""
-read(long_string,*,iostat=ierr) word
-nwords = count(word/="")
-!Loop to check for punctuations
-do i=0,len(long_string)
-select case(long_string(i))
-      case(".")
-      sentence = sentence + 1
-      case("?")
-      sentence = sentence + 1
-      case("!")
-      sentence = sentence + 1
-      case(";")
-      sentence = sentence + 1
-      case(":")
-      sentence = sentence + 1
-end select
-end do
-!Counts the vowels
-do j=0,len(long_string)
-select case(long_string(j))
-      case('a','e','i','o','u','y','A','E','I','O','U','Y')
-      syllable = syllable + 1
-end select
-end do
-print*,"READ",filesize,"characters."
-alpha = syllable/nwords
-beta = nwords/sentence;
-!Calculations for the index and grade
-findex = 206.835-(alpha*84.6)-(beta*1.015)
-grade = (alpha*11.8)+(beta*.39)-15.59
-print*,"Words: ",nwords
-print*,"Sentences: ", sentence
-print*,"Syllables: ", syllable
-print*,"Index: ", findex
-print*,"Grade: ", grade
-
-end program reader
-
-subroutine read_file(string,filesize)
-
-character,dimension(:),allocatable::string
+character (LEN=5000000)::long_string
+character (LEN=1)::input
 integer::counter
-integer::filesize
-character(LEN=1)::input
 character(LEN=50)::arg
 !Sets arg equal to the file entered
 call getarg(1,arg)
@@ -73,17 +18,51 @@ if(iargc() == 0) then
       read*, arg
 endif
 
-inquire(file=arg,size=filesize)
 open(unit=5,status="old",access="direct",form="unformatted",recl=1,file=arg)
-allocate(string(filesize))
 
 counter=1
 100 read(5,rec=counter,err=200)input
-      string(counter:counter)=input
+      long_string(counter:counter)=input
       counter=counter+1
       goto 100
 200 continue
 counter=counter-1
 
 close(5)
-end subroutine read_file
+
+!Counts words
+nwords=0
+pos=1
+loop:do 
+      i = verify(long_string(pos:), ' ')
+      if(i == 0) exit loop
+      nwords = nwords + 1
+      pos = pos + i - 1
+      i = scan(long_string(pos:), ' ')
+      if(i == 0 ) exit loop
+      pos = pos + i - 1
+end do loop
+pos2=1
+nsentence=0
+!Loop to check for punctuations
+loop:do
+      j = verify(long_string(pos2:), '.?!;:')
+      if(j == 0) exit loop
+      nsentence = nsentence + 1
+      pos2 = pos2 + j - 1
+      j = scan(long_string(pos2:), '.?!;:')
+      if(j == 0 ) exit loop
+       pos2 = pos2 + j - 1
+end do loop
+alpha = nsyllable/nwords
+beta = nwords/nsentence;
+!Calculations for the index and grade
+findex =float(int((206.835-(alpha*84.6)-(beta*1.015))*1000.0+0.5))/1000.0
+grade = (((alpha*11.8)+(beta*.39)-15.59)*1000.0)/1000.0
+print*,"Words: ", nwords
+print*,"Sentences: ", nsentence
+print*,"Syllables: ", nsyllable
+print*,"Index: ", findex
+print*,"Grade: ", grade
+
+end program reader
