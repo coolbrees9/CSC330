fc1dd90da534fa37281689e738e70fba24c66e50
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-09-22 14:33:01 -0400

    Final Fortran

--- fortran/parse4.f90
+++ fortran/parse4.f90
@@ -4,8 +4,8 @@ implicit none
 integer::filesize
 !Variable declarations
 integer::nwords,nsentence,nsyllable,pos,pos2,pos3,i,j,k
-real::alpha, beta, findex, grade
-
+real::alpha, beta, grade
+integer::findex
 character (LEN=5000000)::long_string
 character (LEN=1)::input
 integer::counter
@@ -56,24 +56,26 @@ end do loop2
 pos3=1
 nsyllable=0
 loop3: do
-      k = verify(long_string(pos3:), 'aeiouyAEIOUY')
+      k = verify(long_string(pos3:), 'aeiouy')
       if(k == 0) exit loop3
       nsyllable = nsyllable + 1
       pos3 = pos3 + k - 1
-      k = scan(long_string(pos3:), 'aeiouyAEIOUY')
+      k = scan(long_string(pos3:), 'aeiouy')
       if(k == 0 ) exit loop3
       pos3 = pos3 + k - 1
 end do loop3
 
-alpha = nsyllable/nwords
-beta = nwords/nsentence
+alpha = real(nsyllable)/real(nwords)
+beta = real(nwords)/real(nsentence)
+!print*,"Alpha: ", alpha
+!print*,"Beta: ", beta
 !Calculations for the index and grade
-findex =(((206.835-(alpha*84.6)-(beta*1.015))))
-grade = (((alpha*11.8)+(beta*0.39)-15.59))
-print*,"Words: ", nwords
-print*,"Sentences: ", nsentence
-print*,"Syllables: ", nsyllable
+findex = nint(((206.835-(alpha*84.6)-(beta*1.015))))
+grade = ((alpha*11.8)+(beta*0.39)-15.59)
+!print*,"Words: ", nwords
+!print*,"Sentences: ", nsentence
+!print*,"Syllables: ", nsyllable
 print*,"Index: ", findex
-print*,"Grade: ", grade
+write(*,'("Grade: ", f12.1)') grade
 
 end program flesch
