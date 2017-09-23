c1c963749f96adfc0c020ea63b66ecf35176c5b9
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-09-23 17:41:25 -0400

    Finsihed counts, working on args

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -1,7 +1,5 @@
-with Ada.Text_IO;
-with Ada.IO_Exceptions;
-use Ada.Text_IO;
-use Ada.IO_Exceptions;
+with Ada.Text_IO,Ada.IO_Exceptions,Ada.Command_Line,Ada.Strings,Ada.Strings.Fixed, Ada.Integer_Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
+use Ada.Text_IO,Ada.IO_Exceptions,Ada.Command_Line,Ada.Strings,Ada.Strings.Fixed, Ada.Integer_Text_IO, Ada.Float_Text_IO, Ada.Command_Line;
 
 procedure flesch is
 
@@ -9,70 +7,106 @@ In_File :File_Type;
 value :Character;
 string_array :array(1..5000000)of Character;
 pos :Integer;
-word :Float:= 0.0;
-syllable :Float:= 0.0;
-sentence :Float:= 0.0;
-alpha :Float;
-beta :Float;
-index :Float;
-grade :Float;
-vowel : constant String:="aeiouyAEIOUY";
-
+word, sentence, syllable :Float:= 0.0;
+isWord :boolean:= false;
+alpha, beta, index, grade :Float;
+arg :String(1..25);
+--Function that will get the command line file entered
+--function Argument(Number : in Positive) return String is
+--begin
+--      return arg;
+--end Argument;
+--
 begin
+Open(File=>In_File,Mode=>Ada.Text_IO.In_File,Name=>"KJV.txt");
 
-Ada.Text_IO.Open(File=>In_File,Mode=>Ada.Text_IO.In_File,Name=>"KJV.txt");
-
-pos:=0;
+pos:= 0;
 while not End_of_File(In_File) loop
-      Ada.Text_IO.Get(File=>In_File,Item=>value);
-      pos:=pos+1;
+      Get(File=>In_File,Item=>value);
+      pos := pos + 1;
       string_array(pos):=value;
+      if(End_of_File(In_File)) then
+            pos := pos + 1;
+            string_array(pos) := Character'Val(10);
+            pos := pos + 1;
+      end if;
 end loop;
 
 exception
 when Ada.IO_Exceptions.END_ERROR=>Ada.Text_IO.Close(File=>In_File);
 --Does the word count
-for i in In_File'Range
- loop
-      if In_File(i) = ' ' and Word = true then
-            Word:=False;
-      elsif In_File(i) /= '_' and Word = false then
-            Word:=True;
-      word:=word+1;
+for i in 1..pos loop
+      if (string_array(i) = ' ' and isWord = true) then  --end of word
+            isWord:=False;
+      elsif (string_array(i) /= '_' and isWord = false) then  --beginning of word
+            isWord:=True;
+      word := word + 1.0;
       else
             null;
       end if;
 end loop;
 --Checks for the sentence count
-for i in In_File'Range
- loop
-      if In_File(i) = '.' 
-      or if In_File(i) = '?' 
-      or if In_File(i) = '!' 
-      or if In_File(i) = ';' 
-      or if In_File(i) = ':' then
-           sentence:=sentence+1;
+for i in 1..pos loop
+      if (string_array(i) = '.') or 
+      (string_array(i) = '?') or 
+      (string_array(i) = '!') or 
+      (string_array(i) = ';') or
+      (string_array(i) = ':') then
+           sentence := sentence + 1.0;
       end if;
 end loop;
 --Checks for vowels
-for i in In_File'Range
- loop
-      if In_File(i) = 'a' or 'e' or 'i' or 'u' or 'y' or 'A' or 'E' or 'I' or 'O' or 'U' or 'Y' then
-            syllable:=syllable+1;
+for i in 1..pos loop
+      if (string_array(i) = 'a') or 
+      (string_array(i) = 'e') or 
+      (string_array(i) = 'i') or
+      (string_array(i) = 'o') or 
+      (string_array(i) = 'u') or 
+      (string_array(i) = 'y') or
+      (string_array(i) = 'A') or 
+      (string_array(i) = 'E') or 
+      (string_array(i) = 'I') or 
+      (string_array(i) = 'O') or 
+      (string_array(i) = 'U') or 
+      (string_array(i) = 'Y') then
+            syllable := syllable + 1.0;
+            if (string_array(i) = 'a') or
+            (string_array(i+1) = 'e') or
+            (string_array(i+1) = 'i') or
+            (string_array(i+1) = 'o') or
+            (string_array(i+1) = 'u') or
+            (string_array(i+1) = 'y') or
+            (string_array(i+1) = 'A') or
+            (string_array(i+1) = 'E') or
+            (string_array(i+1) = 'I') or
+            (string_array(i+1) = 'O') or
+            (string_array(i+1) = 'U') or
+            (string_array(i+1) = 'Y') then
+                  syllable := syllable - 1.0;
+            elsif (string_array(i+1) = ' ' and string_array(i) = 'e') then
+                  syllable := syllable + 1.0;
+            end if;
       end if;
 end loop;
       
---Ada.Text_IO.New_Line;
 --Calculates the variables
-alpha:=syllable/word;
-beta:=word/sentence;
-index:=209.835-(alpha*84.6)-(beta*1.015);
-grade:=(alpha*11.8)+(beta*0.39)-15.59;
+alpha := syllable/word;
+beta := word/sentence;
+index := 209.835-(alpha*84.6)-(beta*1.015);
+grade := (alpha*11.8)+(beta*0.39)-15.59;
 --Prints the variables
-Put_Line(Item=>"Words: " &Float'image(word));
-Put_Line(Item=>"Sentences: " &Float'image(sentence));
-Put_Line(Item=>"Syllables: " &Float'image(syllable));
-Put_Line(Item=>"Index: " &Float'image(index));
-Put_Line(Item=>"Grade: " &Float'image(grade));
+--Put_Line(Item=>"Alpha: " &Float'image(alpha));
+--Put_Line(Item=>"beta: " &Float'image(beta));
+--Put_Line(Item=>"Words: " &Float'image(word));
+--Put_Line(Item=>"Sentences: " &Float'image(sentence));
+--Put_Line(Item=>"Syllables: " &Float'image(syllable));
+Put("Index: ");
+--Converts the index to integer and rounds it
+Ada.Integer_Text_IO.Put(Integer(index));
+Put_Line("");
+Put("Flesch Grade: ");
+--Makes the grade only have 1 decimal place with no exponents
+Ada.Float_Text_IO.Put(grade,aft => 1, exp => 0);
+Put_Line("");
 
 end flesch;
