abb20999680433709a58fc406f3c1546882be037
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2017-09-25 19:36:47 -0400

    Final fortran, added comments

--- fortran/parse4.f90
+++ fortran/parse4.f90
@@ -1,3 +1,4 @@
+!Program that opens file entered and returns the flesch index and grade
 program flesch
 implicit none
 
@@ -6,17 +7,18 @@ integer::filesize
 integer::nwords,nsentence,nsyllable,pos,pos2,pos3,i,j,k
 real::alpha, beta, grade
 integer::findex
-character (LEN=5000000)::long_string
+character (LEN=8000000)::long_string
 character (LEN=1)::input
 integer::counter
 character(LEN=50)::arg
 !Sets arg equal to the file entered
 call getarg(1,arg)
+!If no argument entered goes here
 if(iargc() == 0) then
       print*, "Must input a file."
       read*, arg
 endif
-
+!Opens the file 
 open(unit=5,status="old",access="direct",form="unformatted",recl=1,file=arg)
 
 counter=1
@@ -32,9 +34,9 @@ close(5)
 pos=1
 nwords=0
 loop: do
-      i = verify(long_string(pos:), ' ')
+      i = verify(long_string(pos:), ' ')  !Sees if char at pos is whitespace
       if(i == 0) exit loop
-      nwords = nwords + 1
+      nwords = nwords + 1  !Increments nwords based on whitespace
       pos = pos + i - 1
       i = scan(long_string(pos:), ' ')
       if(i == 0 ) exit loop
@@ -44,9 +46,9 @@ end do loop
 pos2=1
 nsentence=0
 loop2: do
-      j = verify(long_string(pos2:), '.?!;:')
+      j = verify(long_string(pos2:), '.?!;:')  !Sees if char at pos is a punct
       if(j == 0) exit loop2
-      nsentence = nsentence + 1
+      nsentence = nsentence + 1  !Increments nsentence based on puntuations
       pos2 = pos2 + j - 1
       j = scan(long_string(pos2:), '.?!;:')
       if(j == 0 ) exit loop2
@@ -56,15 +58,15 @@ end do loop2
 pos3=1
 nsyllable=0
 loop3: do
-      k = verify(long_string(pos3:), 'aeiouy')
+      k = verify(long_string(pos3:), 'aeiouy')  !Sees if char at pos is a vowel
       if(k == 0) exit loop3
-      nsyllable = nsyllable + 1
+      nsyllable = nsyllable + 1  !Increments nsyllable based on vowel
       pos3 = pos3 + k - 1
       k = scan(long_string(pos3:), 'aeiouy')
       if(k == 0 ) exit loop3
       pos3 = pos3 + k - 1
 end do loop3
-
+!Casts variables as real to have decimal places
 alpha = real(nsyllable)/real(nwords)
 beta = real(nwords)/real(nsentence)
 !print*,"Alpha: ", alpha
@@ -76,6 +78,6 @@ grade = ((alpha*11.8)+(beta*0.39)-15.59)
 !print*,"Sentences: ", nsentence
 !print*,"Syllables: ", nsyllable
 print*,"Index: ", findex
-write(*,'("Grade: ", f12.1)') grade
+write(*,'("Grade: ", f12.1)') grade  !Rounds grade to one decimal place
 
 end program flesch
